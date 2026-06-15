c2dcaa430697d07cac00fa001e8227a606c76d65
balajiNadar08 <balajigittttt@gmail.com>
2026-06-15 23:00:38 +0530

    Updated rss feeds

--- lib/rss/rss-feeds.ts
+++ lib/rss/rss-feeds.ts
@@ -2,43 +2,80 @@ export const RSS_FEEDS = {
   TECHNOLOGY: [
     "https://techcrunch.com/feed/",
     "https://www.theverge.com/rss/index.xml",
+    "https://feeds.bbci.co.uk/news/technology/rss.xml",
+    "https://www.technologyreview.com/stories.rss",
+    "https://news.google.com/rss/search?q=site:medianama.com&hl=en-IN&gl=IN&ceid=IN:en",
   ],
 
   POLITICS: [
     "https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml",
+    "https://news.google.com/rss/search?q=site:reuters.com+politics&hl=en-US&gl=US&ceid=US:en",
+    "https://feeds.bbci.co.uk/news/politics/rss.xml",
+    "https://news.google.com/rss/search?q=site:scroll.in+politics&hl=en-IN&gl=IN&ceid=IN:en",
+    "https://news.google.com/rss/search?q=site:thewire.in&hl=en-IN&gl=IN&ceid=IN:en",
   ],
 
   SCIENCE: [
+    "https://www.nature.com/nature.rss",
     "https://www.sciencedaily.com/rss/all.xml",
+    "https://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+    "https://news.google.com/rss/search?q=site:thehindu.com+science&hl=en-IN&gl=IN&ceid=IN:en",
   ],
 
   SPORTS: [
     "https://www.espn.com/espn/rss/news",
+    "https://feeds.content.dowjones.io/public/rss/rsssportsfeed",
+    "https://sports.ndtv.com/rss/all",
+    "https://news.google.com/rss/search?q=site:theathletic.com+sports&hl=en-US&gl=US&ceid=US:en",
+    "https://news.google.com/rss/search?q=site:wisden.com+india&hl=en-IN&gl=IN&ceid=IN:en",
   ],
 
   BUSINESS: [
-    "https://feeds.a.dj.com/rss/RSSMarketsMain.xml",
+    "https://feeds.content.dowjones.io/public/rss/WSJcomUSBusiness",
+    "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10001147",
+    "https://rss.nytimes.com/services/xml/rss/nyt/Business.xml",
+    "https://news.google.com/rss/search?q=site:the-ken.com&hl=en-IN&gl=IN&ceid=IN:en",
   ],
 
   HEALTH: [
-    "https://www.medicalnewstoday.com/rss",
+    "https://feeds.bbci.co.uk/news/health/rss.xml",
+    "https://rss.nytimes.com/services/xml/rss/nyt/Health.xml",
+    "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000108",
+    "https://news.google.com/rss/search?q=site:scroll.in+pulse+OR+health&hl=en-IN&gl=IN&ceid=IN:en",
+  ],
+
+  FINANCE: [
+    "https://feeds.content.dowjones.io/public/rss/RSSMarketsMain",
+    "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=10000664",
+    "https://news.google.com/rss/search?q=site:ft.com+markets&hl=en-GB&gl=GB&ceid=GB:en",
+    "https://news.google.com/rss/search?q=site:livemint.com+markets&hl=en-IN&gl=IN&ceid=IN:en",
+  ],
+
+  TRAVEL: [
+    "https://rss.nytimes.com/services/xml/rss/nyt/Travel.xml",
+    "https://moxie.foxnews.com/google-publisher/travel.xml#&_intcmp=fnc_story_article_main-content_article-body_6_7",
+  ],
+
+  ENVIRONMENT: [
+    "https://rss.nytimes.com/services/xml/rss/nyt/Climate.xml",
+    "https://feeds.bbci.co.uk/news/science_and_environment/rss.xml",
+    "https://news.google.com/rss/search?q=site:downtoearth.org.in&hl=en-IN&gl=IN&ceid=IN:en",
+  ],
+
+  EDUCATION: [
+    "https://news.google.com/rss/search?q=site:timeshighereducation.com+education&hl=en-US&gl=US&ceid=US:en",
+    "https://feeds.bbci.co.uk/news/education/rss.xml",
+    "https://news.google.com/rss/search?q=site:careers360.com&hl=en-IN&gl=IN&ceid=IN:en",
   ],
 
   ENTERTAINMENT: [
     "https://variety.com/feed/",
+    "https://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml",
+    "https://news.google.com/rss/search?q=site:filmcompanion.in&hl=en-IN&gl=IN&ceid=IN:en",
   ],
 
-  FINANCE: [
-    "https://www.marketwatch.com/rss/topstories",
+  CELEBRITY: [
+    "https://people.com/celebrity/feed/",
+    "https://news.google.com/rss/search?q=site:thehindu.com+entertainment&hl=en-IN&gl=IN&ceid=IN:en",
   ],
-
-  TRAVEL: [],
-
-  EDUCATION: [
-    "https://indianexpress.com/section/education/feed/",
-  ],
-
-  ENVIRONMENT: [],
-
-  CELEBRITY: [],
 } as const;
